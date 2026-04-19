ab042800022a47201fde2049e0dfb4ad6dab2e04
Matt-Dinoff <mdinoff@kennesaw.edu>
2026-04-18 22:25:26 -0400

    adding txt files

--- TheCedwickCurse.py
+++ TheCedwickCurse.py
@@ -16,32 +16,10 @@ dialogue_list= []
     #print("You have made these choices:", choices)
     #print("You decided to say this: ", dialouge_list)
 
-#def intro():
-# text = "There was once a mansion that belonged to a family in the early 1900s, the Cedwicks."
-# print(textwrap.fill(text, width=80))
-# print()
-# text = "As their wealth grew, however, so did their greed."  
-# print(textwrap.fill(text, width=80))
-# text = "The Cedwick family had Frank, Jane and Aelita their daughter. They had maids and landscapers take care of the house and of Aeita, while Frank and Jane went away on vacation after vacation or to the next factory site."
-# print(textwrap.fill(text, width=80))
-# text ="On Hallow's Eve, Frank's company broke ground on a new factory, a top of an ancient Burial ground."
-# print(textwrap.fill(text, width=80))
-# print()
-# s1 = "'You have trespassed on our land and tarnished out sacred place!', an old ghastly witch doctor shouted at Frank."
-# for character in s1
-# print(textwrap.fill(text, width=80))
-# print()
-# text ="'Forever and on you shall never die. Until a man with a fear of snakes comes and this curse will live on foreever!'"
-# print(textwrap.fill(text, width=80))
-# print()
-# text = "The witch doctor raised her arms, swirled her staff around as a bolt of lightning crashed down from the sky and struck Frank."
-# print(textwrap.fill(text,width=80))
-# print()
-# text = "Nothing was left of them. Aelita inherited the business but died tragically of starvation once the maids absconded with the Cedwick’s wealth. Her family roamed their ill-fated house as spectres, desperate to save what they had left of their ‘fortune’."
-# print(textwrap.fill(text,width=80))
-# print()
-
-intro_doc = open("0_intro.txt","r")
+intro_doc = open("0_intro.txt","r", encoding="utf-8")
 intro_text = intro_doc.read()
-intro_text.close
-print(intro_text)
+paragraphs = intro_text.split("\n\n")
+
+for p in paragraphs:
+    print(textwrap.fill(p, width=80))
+    print()  # blank line between paragraphs
